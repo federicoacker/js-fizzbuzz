0f2b2c25b615d1e519ade2c75b6bd7acfccc9aca
federicoacker <acker.federico@gmail.com>
2026-03-24 13:45:59 +0100

    Aggiunto parte di codice basata su scaffolding logico

--- scripts/script.js
+++ scripts/script.js
@@ -4,9 +4,31 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 // Fase 1: creo una variabile che sia modificabile che sarà quella che viene cambiata e poi stampata.
+let parola = '';
 // Fase 2: Comincio a pensare al ciclo for, è abbastanza semplice, deve solo andare da 1 a 100;
-// Fase 3: Comincio a pensare ai condizionali, abbiamo, SE l'indice del nostro for, quindi i, % 3 === 0 allora aggiungeremo 
-// Fizz alla nostra parola
-// Poi in seguito un altro if separato da questo, SE l'indice del nostro for, quindi i, % 5 === 0 allora aggiungeremo Buzz alla nostra parola
-// Alla fine di questi due check, SE la nostra parola non è vuota (ovvero le abbiamo dato Fizz o Buzz o FizzBuzz) 
-// allora stampiamo la parola, altrimenti stampiamo il numero
+for(let i = 1; i <= 100; i++){
+    // Fase 3: 
+    
+    // Comincio a pensare ai condizionali, abbiamo, SE l'indice del nostro for, 
+    // quindi i % 3 === 0 allora aggiungeremo Fizz alla nostra parola
+    if(i % 3 === 0){
+        parola += "Fizz";
+    }
+    // Poi in seguito un altro if separato da questo, SE l'indice del nostro for, 
+    // quindi i % 5 === 0 allora aggiungeremo Buzz alla nostra parola
+    if(i % 5 === 0){
+        parola += "Buzz";
+    }
+    // Alla fine di questi due check, SE la nostra parola non è vuota (ovvero le abbiamo dato Fizz o Buzz o FizzBuzz) 
+    // allora stampiamo la parola, altrimenti stampiamo il numero
+    if(parola.length !== 0){ //La parola non è vuota, quindi le è stato assegnato Fizz, Buzz o FizzBuzz
+        console.log(parola);
+        //Riassegnamo la parola a una stringa vuota per ricominciare il nostro ciclo
+        parola = '';
+    }
+    else{ // La parola è vuota, quindi stampiamo il numero
+        console.log(i);
+    }
+
+
+}
