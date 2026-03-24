e16af8984a6f0e9f1f49a6f0cb36577cb584dd89
federicoacker <acker.federico@gmail.com>
2026-03-24 14:27:16 +0100

    Provato a riscrivere quello delle prove strane facendogli fare step da 3 in modo da ridurre il numero di iterazioni

--- Bonus (Quando corto riesco a scriverlo)/scriptProveStrane.js
+++ Bonus (Quando corto riesco a scriverlo)/scriptProveStrane.js
@@ -1,6 +1,12 @@
-let i = 1;
-while(i <= 100){
-    // mettere i++ dentro questo console comand con il ternario non fa l'assegnazione effettiva per i quindi poi diventa un ciclo infinito
-    console.error((i % 3 === 0 && i % 5 === 0) ? 'FizzBuz' : ( i % 3 === 0) ? 'Fizz' : ( i % 5 === 0) ? 'Buzz' : i); 
-    i++;
+//Prova Strana fatta facendo saltelli di 3 per volta negli step del for, per vedere se può essere più efficiente in quanto fa meno cicli
+
+for (let i = 0; i <= 100; i += 3) {
+    if (i >= 1) {
+        console.log(((i - 2) % 5 === 0) ? 'Buzz' : ((i - 2) > 0) ? i - 2 : '');
+        console.log(((i - 1) % 5 === 0) ? 'Buzz' : ((i - 1) > 0) ? i - 1 : '');
+        console.log((i % 5 === 0) ? 'FizzBuzz' : 'Fizz');
+    }
+    if(i === 99){
+        console.log('Buzz');
+    }
 }
