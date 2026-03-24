b39e013dd0eff8876815df9d0618bec728e4630e
federicoacker <acker.federico@gmail.com>
2026-03-24 14:46:27 +0100

    Aggiunto un altra prova a scriptProveStrane.js dove uso step di 5 per il ciclo for in modo da fare solo 20 iterazioni

--- Bonus (Quando corto riesco a scriverlo)/scriptProveStrane.js
+++ Bonus (Quando corto riesco a scriverlo)/scriptProveStrane.js
@@ -1,5 +1,9 @@
+console.log(`
+    /////////////////////////////////////
+    Prova con step da 3:
+    /////////////////////////////////////
+    `);
 //Prova Strana fatta facendo saltelli di 3 per volta negli step del for, per vedere se può essere più efficiente in quanto fa meno cicli
-
 for (let i = 0; i <= 100; i += 3) {
     if (i >= 1) {
         console.log(((i - 2) % 5 === 0) ? 'Buzz' : ((i - 2) > 0) ? i - 2 : ''); // Controlliamo se il numero 2 posti prima di i è divisibile per 5 ( e anche se è > 0)
@@ -11,5 +15,23 @@ for (let i = 0; i <= 100; i += 3) {
     }
     if(i === 99){                                                               // Arrivato a 99, non possiamo fare step di 3 oltre, quindi per il 100 lo stampiamo manualmente
         console.log('Buzz');
+    }
+} 
+console.log(`
+    /////////////////////////////////////
+    Prova con step da 5:
+    /////////////////////////////////////
+    `);
+//Proviamo ancora più strano, step da 5 così faccio solo 20 iterazioni
+for(let i = 0; i<= 100; i += 5) //Step di 5 quindi sempre Buzz di sicuro.
+{
+    if ( i >= 5){ //Dal Primo step in poi, facciamo i controlli su tutti gli altri numeri;
+        console.log(((i-4) % 3 == 0) ? 'Fizz' : i-4);
+        console.log(((i-3) % 3 == 0) ? 'Fizz' : i-3);
+        console.log(((i-2) % 3 == 0) ? 'Fizz' : i-2);
+        console.log(((i-1) % 3 == 0) ? 'Fizz' : i-1);
+    }
+    if(i !== 0){
+        console.log((i % 3 === 0) ? 'FizzBuzz' : 'Buzz'); // Caso base, se è divisibile anche per 3, è un fizzbuzz altrimenti è un Fizz;
     }
 }
